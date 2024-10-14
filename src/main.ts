97688dc70a5ad44082058e6ec18226fb0622c309
Daril Marra <daril.marra@gft.com>
2024-10-14 10:34:27 +0200

    msw setup + proxy

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+async function enableMocking() {
+  //TODO: do this only for local development environment
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  const { worker } = await import('./mocks/browser')
+  return worker.start()
+}
+
+const bootstrap = () => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}
+
+enableMocking()
+  .finally(bootstrap);
+
+
